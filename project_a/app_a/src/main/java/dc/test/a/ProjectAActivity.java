d35531c8d752ded5cacadcb95e12a5b2ef203e19
senRsl <dong_M@yeah.net>
2020-11-02 15:43:43 +0800

    a include i j p q;

--- project_a/app_a/src/main/java/dc/test/a/ProjectAActivity.java
+++ project_a/app_a/src/main/java/dc/test/a/ProjectAActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 import dc.test.b.ModuleBActivity;
+import dc.test.i.ModuleIActivity;
+import dc.test.j.ModuleJActivity;
+import dc.test.p.ModulePActivity;
+import dc.test.q.ModuleQActivity;
 
 /**
  * @author senrsl
@@ -28,6 +32,36 @@ public class ProjectAActivity extends Activity {
                 startActivity(new Intent(ProjectAActivity.this, ModuleBActivity.class));
             }
         }, 1000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ProjectAActivity.this, ModuleIActivity.class));
+            }
+        }, 2000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ProjectAActivity.this, ModuleJActivity.class));
+            }
+        }, 3000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ProjectAActivity.this, ModulePActivity.class));
+            }
+        }, 4000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ProjectAActivity.this, ModuleQActivity.class));
+            }
+        }, 5000);
+
+
     }
 
 }
